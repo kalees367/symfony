1583e49ead96916e5c628c289ae0443f27e5dbc0
kaleeswaran.kannaiyan <kalees367@gmail.com>
2018-08-29 22:15:46 +0530

    user list - view user - add new symfony pages update

--- symfony-project/src/Controller/UserController.php
+++ symfony-project/src/Controller/UserController.php
@@ -2,11 +2,16 @@
 // src/Controller/UserController.php
 namespace App\Controller;
 
+use App\Document\User;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
-class UserController extends AbstractController
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+class UserController extends Controller
 {
 	
 	/**
@@ -14,51 +19,94 @@ class UserController extends AbstractController
     */
     public function list()
     {
-        
-		$url = "http://localhost:3000/users/";
-		$contents = file_get_contents($url);
-		$clima=json_decode($contents);
-		// return $clima;
-      /* return new Response(
-            '<html><body>Lucky number: '.$clima->result[0]['firstName'].'</body></html>'
-        ); */
-		return $this->render('user/index.html.twig', [
-            'data'=> $clima->result
-        ]);
+        $db = $this->get('doctrine_mongodb')->getManager();
+        $repository = $db->getRepository(User::class);
+        $users = $repository->findAll();        
+        $db->flush();
+        return $this->render('user/index.html.twig', [
+            'data'=> $users
+        ]); 
+
     }
 	/**
-    * @Route("/user/show/{userid}",requirements={"userId"="\d+"})
+    * @Route("/user/show/{slug}")
     */
 	
-    public function show()
+    public function show($slug)
     {
-        $url = "http://localhost:3000/users/1";
+       /* $url = "http://localhost:3000/users/1";
 		$contents = file_get_contents($url);
 		$clima=json_decode($contents);
-		// return $clima;
-      /* return new Response(
-            '<html><body>Lucky number: '.$clima->result[0]['firstName'].'</body></html>'
-        ); */
 		return $this->render('user/show.html.twig', [
             'data'=> $clima->result
-        ]);
+        ]); */
+        $db = $this->get('doctrine_mongodb')->getManager();
+        $repository = $db->getRepository(User::class);
+        $users = $repository->find(['id' => $slug]);        
+        $db->flush(); 
+        //echo "<pre>";print_R($users);  
+        return $this->render('user/show.html.twig', [
+            'user'=> $users
+        ]); 
+        
     }
-	
-	
-	
 	/**
     * @Route("/user/new")
     */
-    public function new()
+    public function new(Request $request)
     {
-        $number = random_int(0, 100);
+        $user = new User();
+        $form = $this->createFormBuilder($user)
+        ->add('firstName', TextType::class)
+        ->add('lastName', TextType::class)
+        ->add('email', TextType::class)
+        ->add('mobile', TextType::class)
+        ->add('dateofBirth', TextType::class)
+        ->add('education', TextType::class)
+        ->add('bloodGroup', TextType::class)
+        ->add('gender', TextType::class)
+        ->add('save', SubmitType::class, array('label' => 'Add User'))
+        ->getForm();
+        $form->handleRequest($request);
 
-       /* return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        ); */
-		return $this->render('user/new.html.twig', [
-            'number' => $number,
-        ]);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $user = $form->getData();
+            $number = random_int(0, 100);
+            // ... perform some action, such as saving the task to the database
+            // for example, if Task is a Doctrine entity, save it!
+             $entityManager = $this->getDoctrine()->getManager();
+             $entityManager->persist($user);
+             $entityManager->flush();
+             return $this->render('user/edit.html.twig', [
+                'number' => $number,
+            ]);
+           // return $this->redirectToRoute('task_success');
+        }
+
+
+
+        return $this->render('user/new.html.twig', array(
+            'form' => $form->createView(),
+        ));
+        /*
+        $Email = ["kalees@gmail.com","eswar@gmail.com"];
+        $Mobile = ["9943012345","9894125636"];
+        $EduDetails = ["UG"=>"B.Tech","PG"=>"M.tech"];
+        $user->setFirstName("kalees");
+        $user->setLastName("waran");
+        $user->setEmail($Email);
+        $user->setMobile($Mobile);
+        $user->setDateofBirth("05/05/1988");
+        $user->setEducation($EduDetails);
+        $user->setBloodGroup("B+ve");
+        $user->setGender("Male");
+        $db = $this->get('doctrine_mongodb')->getManager();
+
+        $db->persist($user);
+        $db->flush();
+        return new Response( 'ok' ); */
     }
 	/**
     * @Route("/user/edit")
---
+++ symfony-project/src/Document/User.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace App\Document;
+
+use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @MongoDB\Document
+ */
+class User
+{
+    /**
+     * @MongoDB\Id
+     */
+    protected $id;
+
+    /**
+     * @MongoDB\Field(type="string")
+     */
+
+    protected $firstName;
+
+    /**
+     * @MongoDB\Field(type="string")
+     */
+    protected $lastName;
+
+    /**
+     * @MongoDB\Field(type="hash")
+     */
+    protected $email;
+
+    /**
+     * @MongoDB\Field(type="hash")
+     */
+
+    protected $mobile;
+
+    /**
+     * @MongoDB\Field(type="string")
+     */
+    protected $dateofBirth;
+
+    /**
+     * @MongoDB\Field(type="hash")
+     */
+    protected $education;
+
+    /**
+     * @MongoDB\Field(type="string")
+     */
+    protected $bloodGroup;
+
+    /**
+     * @MongoDB\Field(type="string")
+     */
+    protected $gender;
+    
+    /**
+     * @MongoDB\Field(type="date")
+     */
+    protected $created_date;
+
+    public function getId() {
+        return $this->id;
+    }
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+    }
+
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+    }
+
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+    
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setMobile($mobile)
+    {
+        $this->mobile = $mobile;
+    }
+
+    public function getMobile()
+    {
+        return $this->mobile;
+    }
+
+    public function setDateofBirth($dateofBirth)
+    {
+        $this->dateofBirth = $dateofBirth;
+    }
+
+    public function getDateofBirth()
+    {
+        return $this->dateofBirth;
+    }
+
+    public function setEducation($education)
+    {
+        $this->education = $education;
+    }
+
+    public function getEducation()
+    {
+        return $this->education;
+    }
+
+    public function setBloodGroup($bloodGroup)
+    {
+        $this->bloodGroup = $bloodGroup;
+    }
+
+    public function getBloodGroup()
+    {
+        return $this->bloodGroup;
+    }
+
+    public function setGender($gender)
+    {
+        $this->gender = $gender;
+    }
+
+    public function getGender()
+    {
+        return $this->gender;
+    }
+}
